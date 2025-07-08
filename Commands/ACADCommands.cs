3ae17ddb71b6783ebcb5471a9bee003bd5a3327b
Alisa Vinogradova <avinogradova@kesslersolutions.de>
2025-07-08 17:30:13 +0200

    create a mockup fÃor xaml

---
+++ Commands/ACADCommands.cs
@@ -0,0 +1,16 @@
+﻿using ACADTools.Views;
+using Autodesk.AutoCAD.Runtime;
+using app = Autodesk.AutoCAD.ApplicationServices.Application;
+
+namespace ACADTools.Commands
+{
+    public class ACADCommands
+    {
+        [CommandMethod("ACADTOOLS")]
+        public void ModelessWpfDialogCmd()
+        {
+            var dialog = new MainView();
+            var result = app.ShowModalWindow(dialog);
+        }
+    }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ACADTools.ViewModels
+﻿namespace ACADTools.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel
     {
+        public MainViewModel()
+        {
+
+        }
     }
 }
